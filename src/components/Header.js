295b5ecba418989ba041145486e09a501bc8d064
nguyenCD <dinhnguyen.k15@gmail.com>
2022-05-06 09:34:25 +0700

    [Implement] - Implement Header component

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Avatar, IconButton } from "@material-ui/core";
+import Logo from "../img/logo-gmail.png";
+import SearchIcon from "@material-ui/icons/Search";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import "../css/header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img src={Logo} alt="" />
+      </div>
+
+      <div className="header__middle">
+        <SearchIcon />
+        <input placeholder="Search null" type="text" />
+        <ArrowDropDownIcon className="header__inputCaret" />
+      </div>
+
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <Avatar />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
